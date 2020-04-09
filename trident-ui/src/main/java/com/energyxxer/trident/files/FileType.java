e282b71af86e70e1c2c16901fdb8cae844b47d8f
Energyxxer <energyxxer@gmail.com>
2020-04-08 23:55:06 -0400

    Add quick option to create resource pack root in project context menu

--- trident-ui/src/main/java/com/energyxxer/trident/files/FileType.java
+++ trident-ui/src/main/java/com/energyxxer/trident/files/FileType.java
@@ -1,10 +1,15 @@
 package com.energyxxer.trident.files;
 
 import com.energyxxer.commodore.functionlogic.functions.Function;
-import com.energyxxer.trident.ui.dialogs.file_dialogs.*;
+import com.energyxxer.trident.main.window.TridentWindow;
+import com.energyxxer.trident.ui.dialogs.file_dialogs.FileDialog;
+import com.energyxxer.trident.ui.dialogs.file_dialogs.FolderDialog;
+import com.energyxxer.trident.ui.dialogs.file_dialogs.FunctionDialog;
+import com.energyxxer.trident.ui.dialogs.file_dialogs.ProjectDialog;
 import com.energyxxer.trident.ui.styledcomponents.StyledMenuItem;
 
 import java.io.File;
+import java.nio.file.Paths;
 import java.util.function.Predicate;
 
 /**
@@ -19,7 +24,15 @@ public enum FileType {
     JSON(1, "JSON File", "json", ".json", FileDialog::create, (pr, pth) -> true),
     FILE(2, "File", "file", "", FileDialog::create, (pr, pth) -> true),
     FOLDER(2, "Folder", "folder", null, FolderDialog::create, (pr, pth) -> true),
-    PROJECT(3, "Project", "project", null, ProjectDialog::create, (pr, pth) -> true);
+    PROJECT(3, "Project", "project", null, ProjectDialog::create, (pr, pth) -> true),
+    DATA_ROOT(4, "Data Pack Root", "data", null, (type, dest) -> {
+        Paths.get(dest).resolve("datapack").toFile().mkdirs();
+        TridentWindow.projectExplorer.refresh();
+    }, (pr, pth) -> pth.equals(pr) && !Paths.get(pr).resolve("datapack").toFile().exists()),
+    RESOURCES_ROOT(4, "Resource Pack Root", "resources", null, (type, dest) -> {
+        Paths.get(dest).resolve("resources").toFile().mkdirs();
+        TridentWindow.projectExplorer.refresh();
+    }, (pr, pth) -> pth.equals(pr) && !Paths.get(pr).resolve("resources").toFile().exists());
 
     public final int group;
     public final String name;
