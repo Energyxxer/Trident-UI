d74d89b7556d7b1fa4a9569c1fcd74b357e4518d
Energyxxer <energyxxer@gmail.com>
2020-08-07 17:46:58 -0400

    Fix indentation edits not behaving correctly with long lines of text (again)

--- trident-ui/src/main/java/com/energyxxer/trident/ui/editor/behavior/editmanager/edits/IndentEdit.java
+++ trident-ui/src/main/java/com/energyxxer/trident/ui/editor/behavior/editmanager/edits/IndentEdit.java
@@ -19,8 +19,8 @@ public class IndentEdit extends Edit {
     /**
      * ArrayList containing information about how to undo the edit.
      * Must contain even entries.
-     * Every even index (0, 2, 4...) contains the position in the document where spaces were added/removed.
-     * Every odd index (1, 3, 5...) contains the amount of spaces added/removed at the position given by the index before it.
+     * Every even index (0, 2, 4...) contains the position in the document where spaces were added/removed. Doesn't account for indices drifted by adding or removing spaces.
+     * Every odd index (1, 3, 5...) contains the amount of spaces added/removed at the position given by the index before it. (negatives for removal, positives for insertion)
      * */
     private ArrayList<Integer> modifications = new ArrayList<>();
     private final boolean reverse;
@@ -45,27 +45,21 @@ public class IndentEdit extends Edit {
             String text = doc.getText(0, doc.getLength()); //Result
             this.modifications.clear();
 
-            int characterDrift = 0;
-
             CaretProfile nextProfile = new CaretProfile(previousProfile);
 
             int previousL = -1;
 
             for (int i = 0; i < previousProfile.size()-1; i += 2) {
                 //Get bounds of the line to move
-                int selectionStart = previousProfile.get(i) + characterDrift;
-                int selectionEnd = previousProfile.get(i + 1) + characterDrift;
+                int selectionStart = previousProfile.get(i);
+                int selectionEnd = previousProfile.get(i + 1);
 
                 int start = new Dot(Math.min(selectionStart,selectionEnd),editor).getRowStart();
                 int end = new Dot(Math.max(selectionStart,selectionEnd),editor).getRowEnd();
 
-                int spacesChanged = 0;
-
-                int thisSelectionCharacterDrift = 0;
-
                 boolean firstLineOfSelection = true;
 
-                for(int l = start; l < end + thisSelectionCharacterDrift; previousL = l, l = new Dot(l, editor).getRowEnd()+1) {
+                for(int l = start; l < end; previousL = l, l = new Dot(l, editor).getRowEnd()+1) {
                     if(l == previousL) {
                         if(firstLineOfSelection) {
                             firstLineOfSelection = false;
@@ -75,44 +69,45 @@ public class IndentEdit extends Edit {
                         }
                     }
                     firstLineOfSelection = false;
-                    int spaces = StringUtil.getSequenceCount(text," ", l - characterDrift);
+                    int spaces = StringUtil.getSequenceCount(text," ", l);
                     if(!reverse) {
                         int spacesToAdd = 4 - (spaces % 4);
                         spacesToAdd = (spacesToAdd > 0) ? spacesToAdd : 4;
-                        doc.insertString(l,StringUtil.repeat(" ", spacesToAdd),null);
                         modifications.add(l);
                         modifications.add(spacesToAdd);
-                        nextProfile.pushFrom(l,spacesToAdd);
                         actionPerformed = true;
-                        if(l >= end + thisSelectionCharacterDrift) break;
-                        characterDrift += spacesToAdd;
-                        thisSelectionCharacterDrift += spacesToAdd;
-                        spacesChanged += spacesToAdd;
                     } else {
                         if(spaces == 0) {
                             continue;
                         }
                         int spacesToRemove = (spaces % 4 == 0) ? 4 : spaces % 4;
                         if(spacesToRemove != 0) {
-                            nextProfile.pushFrom(l,Math.min(0,-spacesToRemove));
                         }
                         actionPerformed = true;
-                        doc.remove(l,spacesToRemove);
                         modifications.add(l);
-                        modifications.add(spacesToRemove);
-                        if(l >= end + thisSelectionCharacterDrift) break;
-                        characterDrift -= spacesToRemove;
-                        thisSelectionCharacterDrift -= spacesToRemove;
-                        spacesChanged -= spacesToRemove;
+                        modifications.add(-spacesToRemove);
                     }
                 }
-
-                final int fsc = spacesChanged;
-
-                editor.registerCharacterDrift(o -> (o >= start) ? o + fsc : o);
             }
 
-            caret.setProfile(nextProfile);
+            if(actionPerformed) {
+                for(int i = modifications.size()-2; i >= 0; i -= 2) {
+                    int index = modifications.get(i);
+                    int spaces = modifications.get(i+1);
+
+                    if(spaces > 0) {
+                        doc.insertString(index, StringUtil.repeat(" ", spaces), null);
+                        nextProfile.pushFrom(index,spaces);
+                    } else {
+                        doc.remove(index, -spaces);
+                        nextProfile.pushFrom(index,Math.min(0,spaces));
+                    }
+
+                    editor.registerCharacterDrift(o -> (o >= index) ? o + spaces : o);
+                }
+
+                caret.setProfile(nextProfile);
+            }
         } catch(BadLocationException x) {
             x.printStackTrace();
         }
@@ -125,14 +120,17 @@ public class IndentEdit extends Edit {
         EditorCaret caret = editor.getCaret();
 
         try {
-            for(int i = modifications.size()-2; i >= 0; i -= 2) {
+            for(int i = 0; i < modifications.size(); i += 2) {
                 int index = modifications.get(i);
                 int spaces = modifications.get(i+1);
 
-                if(!reverse) doc.remove(index, spaces);
-                else doc.insertString(index, StringUtil.repeat(" ", spaces), null);
+                if(spaces > 0) {
+                    doc.remove(index, spaces);
+                } else {
+                    doc.insertString(index, StringUtil.repeat(" ", -spaces), null);
+                }
 
-                editor.registerCharacterDrift(o -> (o >= index) ? o - ((!reverse) ? spaces : -spaces) : o);
+                editor.registerCharacterDrift(o -> (o >= index) ? o - spaces : o);
             }
         } catch(BadLocationException x) {
             x.printStackTrace();
