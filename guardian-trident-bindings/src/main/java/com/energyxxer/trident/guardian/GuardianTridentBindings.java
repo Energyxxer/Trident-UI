bfdf8aa3070ad12fa36edbf407aa60e0fc6a3264
Energyxxer <energyxxer@gmail.com>
2021-09-19 12:33:51 -0400

    Implement Trident-Language changes (relax no command restriction for custom commands)

--- guardian-trident-bindings/src/main/java/com/energyxxer/trident/guardian/GuardianTridentBindings.java
+++ guardian-trident-bindings/src/main/java/com/energyxxer/trident/guardian/GuardianTridentBindings.java
@@ -7,7 +7,6 @@ import com.energyxxer.guardian.main.window.GuardianWindow;
 import com.energyxxer.guardian.ui.commodoreresources.Plugins;
 import com.energyxxer.guardian.ui.dialogs.file_dialogs.FileDialog;
 import com.energyxxer.guardian.ui.theme.ThemeManager;
-import com.energyxxer.prismarine.plugins.syntax.PrismarineMetaLexerProfile;
 import com.energyxxer.trident.Trident;
 import com.energyxxer.trident.guardian.dialogs.FunctionDialog;
 
@@ -32,8 +31,6 @@ public class GuardianTridentBindings {
         Lang.JSON.addExtension(Trident.PROJECT_FILE_NAME.substring(1));
         Lang.JSON.addExtension(Trident.PROJECT_BUILD_FILE_NAME.substring(1));
         Lang.PRISMARINE_SYNTAX.addExtension("tdnmeta");
-        PrismarineMetaLexerProfile.functionNames.add("storeVar");
-        PrismarineMetaLexerProfile.functionNames.add("storeFlat");
 
         ThemeManager.registerSyntaxThemeFromJar("Trident Syntax Dark");
         ThemeManager.registerSyntaxThemeFromJar("NBTTM Syntax Dark");
